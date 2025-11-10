287916ac23f155765b69512ac533bec8d48632df
ByFloxi <floxi.contactpro@gmail.com>
2025-11-10 13:44:25 +0100

    1.0.8

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /**
- * @author Luuxis
- * Luuxis License v1.0 (voir fichier LICENSE pour les détails en FR/EN)
+ * @author ByFloxi
+ * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0
  */
 
 const { app, ipcMain, nativeTheme } = require('electron');
